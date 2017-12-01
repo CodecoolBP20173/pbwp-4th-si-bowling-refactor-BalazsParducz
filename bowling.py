ee34798297d05abd8ce47f50e7f925f85750e8e9
BalazsParducz <parducz.balazs@gmail.com>
2017-12-01 14:16:07 +0100

    add game rules to scores def

--- bowling.py
+++ bowling.py
@@ -3,16 +3,27 @@ def score(game):
     frame = 1
     in_first_half = True
     for i in range(len(game)):
+        
         if game[i] == '/':
             result += 10 - last
+        # If in two tries, he fails to knock them all down,
+        # his score for that frame is the total number of pins knocked down in his two tries.
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
+        
+        
         if frame < 10  and get_value(game[i]) == 10:
+            # If in two tries he knocks them all down,
+            # this is called a “spare” and his score for the frame is ten
+            # plus the number of pins knocked down on his next throw (in his next turn).
+            
             if game[i] == '/':
                 result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
+            # If on his first try in the frame he knocks down all the pins,
+            # this is called a “strike”. 
+            # His turn is over, and his score for the frame is ten
+            # plus the simple total of the pins knocked down in his next two rolls.
+            elif game[i] == 'X' or 'x':
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
@@ -25,9 +36,12 @@ def score(game):
             in_first_half = False
         else:
             in_first_half = True
+        
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
+    
+    # The game score is the total of all frame scores.
     return result
 
 def get_value(char):
