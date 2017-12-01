0570453f10c5e669cefc76c6f3cece5d58f300ae
BalazsParducz <parducz.balazs@gmail.com>
2017-12-01 11:50:01 +0100

    refactor get_value

--- bowling.py
+++ bowling.py
@@ -31,15 +31,17 @@ def score(game):
     return result
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
-        return 10
-    elif char == '-':
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    bonus = ['X', 'x', '/']
+    for nr in numbers:
+        if char == nr:            
+            return int(char)
+        
+    for bs in bonus:    
+        if char == bs:
+            return 10
+        
+    if char == '-':
         return 0
-    else:
-        raise ValueError()
+
+    raise ValueError()
