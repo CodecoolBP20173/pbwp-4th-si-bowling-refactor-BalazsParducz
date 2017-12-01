26769150ebe24563d016ee46714562296f559520
BalazsParducz <parducz.balazs@gmail.com>
2017-12-01 15:19:09 +0100

    delete redundant statement in_first_half == True

--- bowling.py
+++ bowling.py
@@ -2,44 +2,37 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        
-        if game[i] == '/':
+    for i in range(len(game)):  # amíg a game lista tart
+
+        if game[i] == '/':      # ha a listaelem '/', akkor a result = 10 - last értéke
             result += 10 - last
-        # If in two tries, he fails to knock them all down,
-        # his score for that frame is the total number of pins knocked down in his two tries.
+ 
         else:
-            result += get_value(game[i])
+            result += get_value(game[i])       #  ha a listaelem egyéb, akkor a result értéke a get_value visszatérési értéke
         
         
-        if frame < 10  and get_value(game[i]) == 10:
-            # If in two tries he knocks them all down,
-            # this is called a “spare” and his score for the frame is ten
-            # plus the number of pins knocked down on his next throw (in his next turn).
-            
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            # If on his first try in the frame he knocks down all the pins,
-            # this is called a “strike”. 
-            # His turn is over, and his score for the frame is ten
-            # plus the simple total of the pins knocked down in his next two rolls.
-            elif game[i] == 'X' or 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
-        if not in_first_half:
-            frame += 1
-        if in_first_half == True:
-            in_first_half = False
-        else:
-            in_first_half = True
+        if frame < 10  and get_value(game[i]) == 10:    # ha nem az utolsó frame-nél járunk  és a get_value visszatérési értéke 10
+            if game[i] == '/':                          # és ha a lista elem '/'
+                result += get_value(game[i+1])          # akkor a result egyenlő a get_value visszatérési értékével a következő listaelemnél
+            elif game[i] == 'X' or 'x':                 # vagy ha a listaelem  'X' vagy 'x' 
+                result += get_value(game[i+1])          # akkor a result egyenlő a get_value visszatérési értékével a következő listaelemnél
+                if game[i+2] == '/':                    # ha a game lista következő utáni eleme '/'
+                    result += 10 - get_value(game[i+1]) # akkor a result értéke 10 mínusz get_value visszatérési értéke a következő listaelemnél
+                else:                                   # egyéb esetben
+                    result += get_value(game[i+2])      # a result értéke a get_value visszatérési értéke a következő utáni listaelemnél
         
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
-            frame += 1
+        last = get_value(game[i])   # a last értéke a get_value visszatérési értéke az adott listaelemnek megfelelően
+        
+        if not in_first_half:                           # ha nem az első próbálkozás
+            frame += 1                                  # akkor ugorjunk a következő frame-re
+        if in_first_half:                               # ha az első gurításnál tartunk
+            in_first_half = False                       # akkor jöjjön a következő gurítás
+        else:                                           # ha a második gurításnál tartunk, akkor...
+            in_first_half = True                        # állítsuk vissza az elsőre
+        
+        if game[i] == 'X' or game[i] == 'x':            # ha a listaelem 'X' vagy 'x', akkor... 
+            in_first_half = True                        # ... állítsuk a következő elsőgurítás következik...
+            frame += 1                                  # ... a  következő frame-ben
     
     # The game score is the total of all frame scores.
     return result
